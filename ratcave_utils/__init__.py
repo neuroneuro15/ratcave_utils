f6671dc7f5e09b3c0f0cfc1b67b53be808b2beff
Nicholas Del Grosso <delgrosso@bio.lmu.de>
2016-11-17 18:04:26 +0100

    fixed name errors

--- ratcave_utils/__init__.py
+++ ratcave_utils/__init__.py
@@ -2,7 +2,12 @@ import click
 
 @click.group()
 def cli():
-    """Group of command-line-tools, useful for using the ratcave VR setup. Each subcommand can also be called on its own."""
+    """
+    Group of command-line-tools, useful for using the ratcave VR setup. Each subcommand can also be called on its own.
+
+    WARNIING: Don't run these scripts in Powershell on Windows!  For some reason, the calibration results are coming out
+    different in this environment.  Strange but true.
+    """
     pass
 
 from . import arena_scanner
