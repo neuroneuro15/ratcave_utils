873247bd183d7bd1ac89b918574430fb2c123891
Nicholas Del Grosso <delgrosso@bio.lmu.de>
2017-01-31 14:31:21 +0100

    Added trackposition.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
             'ratcave_utils = ratcave_utils:cli',
             'calib_projector = ratcave_utils.calib_projector:calib_projector',
             'trackrotation = ratcave_utils.track_rigidbody:trackrotation',
+            'trackposition = ratcave_utils.track_rigidbody:trackposition',
             'scan_arena = ratcave_utils.arena_scanner:scan_arena',
             'dots_test = ratcave_utils.dots_test:show_dots',
         ],
