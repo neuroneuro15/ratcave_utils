94b8c2f40d3b6092205cc4ddb2314a7bd9d01b39
Nicholas Del Grosso <delgrosso@bio.lmu.de>
2017-04-14 14:54:44 +0200

    changed default settings for visibility mode

--- ratcave_utils/utils/hardware.py
+++ ratcave_utils/utils/hardware.py
@@ -9,7 +9,7 @@ def motive_camera_vislight_configure():
 
         if 'Prime 13' in cam.name:
             cam.settings = motive.CameraSettings(video_mode=2, exposure=33000, threshold=200, intensity=0)
-            cam.image_gain = 8  # 8 is the maximum image gain setting
+            cam.image_gain = 5  # 8 is the maximum image gain setting
             cam.set_filter_switch(False)
         else:
             cam.settings = motive.CameraSettings(video_mode=0, exposure=cam.exposure, threshold=cam.threshold, intensity=cam.intensity)
