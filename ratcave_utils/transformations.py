b17372eb77bea0c9580fef776a865956fcb0fa63
Fabián Štoček <42967098+Fabalinks@users.noreply.github.com>
2020-01-16 17:57:26 +0100

    added ratcave object creation step
    also an example usage!

--- ratcave_utils/transformations.py
+++ ratcave_utils/transformations.py
@@ -1,7 +1,7 @@
 import wavefront_reader
 import numpy as np
 from sklearn.decomposition import PCA
-
+import ratcave as rc
 
 def arena_obj_to_transformation(objfile, object="Arena"):
     """Returns the 3x3 rotation matrix and 1x3 translation array for the "object" object in the objfile, using PCA."""
@@ -11,3 +11,26 @@ def arena_obj_to_transformation(objfile, object="Arena"):
     pca = PCA(n_components=3).fit(floor_verts)
     rotmat, translation = pca.components_, pca.mean_
     return rotmat, translation
+
+
+
+def make_arena_coords_obj(rotmat, translation):
+    """Should (untested!!!) return a ratcave object with the transformation values applied."""
+    mesh = rc.EmptyMesh()
+    mesh.position.xyz = translation
+    mesh.rotation = rc.RotationEuler.from_matrix(rotmat)\
+    return mesh
+
+
+
+# Example usage below (maybe needs some tweaking--untested):
+if __name__ == "__main__":
+    objfile = "arena.obj"
+    arena = rc.read_wavefront(objfile).get_mesh("Arena")
+    
+    rotmat, translation = arena_obj_to_transformation(objfile)
+    exp_transform = make_arena_coords_obj(rotmat, translation)
+    exp_transform.parent = arena
+    
+    
+    
